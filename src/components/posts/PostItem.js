2aab1cefb8fe4bf13ad16e814c96b25c6629f13a
wematuiv <jwematu@idscorpgh.com>
2022-05-03 13:24:59 +0000

    Work appearance of post
    - Redefined the layout of post item
    - Add indicator for number of likes and comments

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -1,21 +1,45 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import {
+	Box,
+	Heading,
+	Text,
+	Divider,
+	Grid,
+	GridItem,
+	Flex
+} from "@chakra-ui/react";
 import PropTypes from "prop-types";
 
 function PostItem({ post, ...rest }) {
-	const { title, body } = post;
+	const { title, body, likes, comments, createdAt } = post;
 
 	return (
-		<Box
+		<Grid
+			flexDirection='column'
 			p={5}
 			shadow='md'
 			bg='#fff'
 			borderRadius={5}
 			borderWidth='1px'
 			{...rest}>
-			<Heading fontSize='xl'>{title}</Heading>
-			<Text mt={4}>{body}</Text>
-		</Box>
+			<Heading fontSize='lg'>{title}</Heading>
+			<Flex flexDir='column'>
+				<Text mt={4}>{body}</Text>
+				<Text fontSize='sm' color='orange.500' alignSelf='flex-end'>
+					{createdAt}
+				</Text>
+			</Flex>
+			{/* <Divider /> */}
+			<Flex>
+				<Text marginRight={3}>{likes} 👍</Text>
+				<Text>{comments || 0} 💬</Text>
+			</Flex>
+			<Divider my={1} />
+			<Flex>
+				<Text marginRight={3}>Like</Text>
+				<Text>{comments} Comment</Text>
+			</Flex>
+		</Grid>
 	);
 }
 
@@ -23,7 +47,10 @@ PostItem.propTypes = { post: PropTypes.object.isRequired };
 PostItem.defaultProps = {
 	post: {
 		title: "example post title",
-		body: "example description"
+		body: "example description",
+		comments: 0,
+		likes: 0,
+		createdAt: "2020-01-01"
 	}
 };
 export default PostItem;
